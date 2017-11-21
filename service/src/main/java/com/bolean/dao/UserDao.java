60600740f45810291c05a11991dd263018d5dd66
wodeln <Lina521521>
2017-11-21 10:02:05 +0800

    删除

--- service/src/main/java/com/bolean/dao/UserDao.java
+++ service/src/main/java/com/bolean/dao/UserDao.java
@@ -9,4 +9,6 @@ public interface UserDao extends JpaRepository<User,Long> {
 //    public List<User> findByName(String name);
 //    public User findOne(Long userId);
     public User findAllByUserId(int userId);
+    
+//    public List<User> findUsersBy
 }
